56227f9bcc9550ad8c55f4744f10ed241a4f84ae
jimwong1023 <jimwong1023@gmail.com>
2019-01-10 20:21:12 -0800

    andria redo

--- assets/glide-slider.js
+++ assets/glide-slider.js
@@ -1,4 +1,29 @@
-new Glide('.glide', {
+// new Glide('.homepage-landing-carousel', {
+//   type: 'carousel',
+//   autoplay: 4000,
+// }).mount();
+
+new Glide('.moissanite-slider', {
   type: 'carousel',
-  autoplay: 4000,
+  focusAt: 'center',
+  breakpoints: {
+    10000: {
+      perView: 10
+    },
+    5000: {
+      perView: 7
+    },
+    2400: {
+      perView: 5
+    },
+    1200: {
+      perView: 4
+    },
+    800: {
+      perView: 3
+    },
+    480: {
+      perView: 2
+    }
+  }
 }).mount();
